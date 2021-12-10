466be58132d277262a15ef656246d3a14ab58ed7
wfercanas <fernando.canas@outlook.com>
2021-12-10 16:58:52 -0500

    add/ letter-spacing and adjust font-size

--- src/components/Titles/styles.js
+++ src/components/Titles/styles.js
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
 
 const StyledSectionTitle = styled.h2`
-  font-size: 1rem;
+  font-size: 0.7rem;
   font-weight: normal;
   color: var(--n200);
   text-transform: uppercase;
+  letter-spacing: 1px;
 `;
 
 export { StyledSectionTitle };
