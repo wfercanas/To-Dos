578aec6fe6e2994bed1342041a969000cd73c486
wfercanas <fernando.canas@outlook.com>
2021-12-10 11:40:43 -0500

    add/ global styles and font icons for sb

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,13 @@
+import '../src/globalStyles.css';
+import '../src/fonts.css';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  layout: 'fullscreen',
+};
