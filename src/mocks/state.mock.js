fa4b1db81ef4bda515674a6d21383d946a5e29c2
wfercanas <fernando.canas@outlook.com>
2022-01-05 11:04:19 -0500

    add/ mock state file

---
+++ src/mocks/state.mock.js
@@ -0,0 +1,27 @@
+const state = {
+  categories: ["Hobby", "Personal", "University"],
+  tasks: [
+    {
+      text: "Cortar cebolla",
+      completed: true,
+      category: "Personal",
+    },
+    {
+      text: "Comprar leche",
+      completed: false,
+      category: "Personal",
+    },
+    {
+      text: "Tomar curso de intro a React",
+      completed: false,
+      category: "University",
+    },
+    {
+      text: "Jugar Fortnite",
+      completed: false,
+      category: "Hobby",
+    },
+  ],
+};
+
+export { state };
