ace80f2170401d7f0afd7ef6b06cd2f62181ab88
wfercanas <fernando.canas@outlook.com>
2022-01-06 11:32:32 -0500

    add/ task id property

--- src/mocks/state.mock.js
+++ src/mocks/state.mock.js
@@ -2,21 +2,25 @@ const state = {
   categories: ["Hobby", "Personal", "University"],
   tasks: [
     {
+      id: 1,
       text: "Cortar cebolla",
       completed: true,
       category: "Personal",
     },
     {
+      id: 2,
       text: "Comprar leche",
       completed: false,
       category: "Personal",
     },
     {
+      id: 3,
       text: "Tomar curso de intro a React",
       completed: false,
       category: "University",
     },
     {
+      id: 4,
       text: "Jugar Fortnite",
       completed: false,
       category: "Hobby",
