b2cba1caceadb1e8553e95986ad9133aaf1298f9
wfercanas <fernando.canas@outlook.com>
2022-01-10 15:53:22 -0500

    remove/ state + functions to context. No props.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,44 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { HomeUI } from "./HomeUI";
-import { useLocalStorage } from "../../hooks/useLocalStorage";
+import { AppProvider } from "../../context";
 
 const Home = () => {
-  const {
-    item: state,
-    saveItem: saveState,
-    loading,
-    error,
-  } = useLocalStorage("STATE_v1", { categories: [], tasks: [] });
-  const [searchValue, setSearchValue] = useState("");
-
-  const filteredTasks = (task) =>
-    task.text.toLowerCase().includes(searchValue.toLowerCase());
-
-  const handleComplete = (id) => {
-    const stateTasks = [...state.tasks];
-    const taskIndex = stateTasks.findIndex((task) => task.id === id);
-    stateTasks[taskIndex].completed = !stateTasks[taskIndex].completed;
-    saveState({ ...state, tasks: stateTasks });
-  };
-
-  const deleteTask = (id) => {
-    let stateTasks = state.tasks.filter((task) => task.id !== id);
-    saveState({ ...state, tasks: stateTasks });
-  };
-
   return (
-    <HomeUI
-      loading={loading}
-      error={error}
-      categories={state.categories}
-      tasks={state.tasks}
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
-      filteredTasks={filteredTasks}
-      handleComplete={handleComplete}
-      deleteTask={deleteTask}
-    />
+    <AppProvider>
+      <HomeUI />
+    </AppProvider>
   );
 };
 
