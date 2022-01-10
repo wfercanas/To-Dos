123513de8fe5e20f6d706099c3ebf3dcf79a2ab1
wfercanas <fernando.canas@outlook.com>
2022-01-10 15:50:50 -0500

    add/ app context and export provider

---
+++ src/context/index.js
@@ -0,0 +1,48 @@
+import React, { useState, createContext } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+const AppContext = createContext();
+
+function AppProvider({ children }) {
+  const {
+    item: state,
+    saveItem: saveState,
+    loading,
+    error,
+  } = useLocalStorage("STATE_v1", { categories: [], tasks: [] });
+  const [searchValue, setSearchValue] = useState("");
+
+  const filteredTasks = (task) =>
+    task.text.toLowerCase().includes(searchValue.toLowerCase());
+
+  const handleComplete = (id) => {
+    const stateTasks = [...state.tasks];
+    const taskIndex = stateTasks.findIndex((task) => task.id === id);
+    stateTasks[taskIndex].completed = !stateTasks[taskIndex].completed;
+    saveState({ ...state, tasks: stateTasks });
+  };
+
+  const deleteTask = (id) => {
+    let stateTasks = state.tasks.filter((task) => task.id !== id);
+    saveState({ ...state, tasks: stateTasks });
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        loading,
+        error,
+        state,
+        searchValue,
+        setSearchValue,
+        filteredTasks,
+        handleComplete,
+        deleteTask,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+}
+
+export { AppContext, AppProvider };
