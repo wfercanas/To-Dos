b1460e8d6c915a481bcf1cc935d2bc6cb5fcb68e
wfercanas <fernando.canas@outlook.com>
2022-01-10 18:10:59 -0500

    add/ Blanket styles and story

---
+++ src/components/Blankets/Blankets.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Blanket } from ".";
+
+export default {
+  title: "Components / Blankets",
+  compotents: Blanket,
+};
+
+export const Modal = () => <Blanket />;
---
+++ src/components/Blankets/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { StyledBlanket } from "./styles";
+
+const Blanket = () => {
+  return <StyledBlanket />;
+};
+
+export { Blanket };
